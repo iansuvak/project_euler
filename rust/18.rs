850264a6a3b6265461f3e72b7ea001e1fbbd9015
Ian Suvak <ian.suvak@gmail.com>
2015-02-09 09:57:28 -0500

    Problem 18 (and 67 since it's the same) - bottom-up algorithm using (unnecessary) structs and VecMap

--- rust/18.rs
+++ rust/18.rs
@@ -1,6 +1,13 @@
 //Key realization here is to start working from the bottom.
 //Since any two neighboring sibling nodes share the same parent one having greater value from the
-//bottom is strictly better than the other
+//bottom is strictly better than the other. We can assess each node by comparing it's children's
+//values and taking the larger one
+//You could do it in many less lines of code by accessing/mutating the initial vector directly
+//instead of constructing nodes and a VecMap but I ended up learning more Rust this way
+
+use std::collections::VecMap;
+use std::iter::range;
+use std::cmp::max;
 
 struct Node {
     depth: usize,
@@ -10,83 +17,40 @@ struct Node {
 }
 
 impl Node {
-    fn getParent(&self, left: bool, input: &Vec<Vec<i32>>) -> Option<Node> {
-        println!("depth:{}, pos:{}, left:{}", self.depth, self.position, left);
-        if self.depth == 0 || (left && self.position == 0) || (!left && self.position == self.depth) {
-          None
-        } else {
-            let depth = self.depth-1;
-            let position = if left {self.position - 1} else {self.position};
-            Some(Node {
-                depth: depth,
-                position: position,
-                val:input[depth][position],
-                children_max_sum: self.children_max_sum + self.val})
-        }
-
+    // TODO:: figure a way to DRY this up.. macros?
+    fn get_child_coords(&self, left: bool) -> (usize, usize) {
+        let depth = self.depth+1;
+        let position = if left {self.position} else {self.position + 1};
+        (depth, position)
     }
 
-    fn max(&self) -> i32 {
+    fn total(&self) -> i32 {
         self.children_max_sum + self.val
     }
-
-    //fn is_sibling_of(&self, candidate: &Node) -> bool {
-        //if self.depth == candidate.depth && ((self.position as isize - candidate.position as isize).abs() == 1) { true } else { false}
-    //}
 }
 
-impl Clone for Node {
-    fn clone(&self) -> Self {
-        Node {
-            depth:self.depth,
-            position:self.position,
-            val:self.val,
-            children_max_sum:self.children_max_sum,
-        }
+// Bijection from N x N -> N so that I can map my 2d coordinated onto my VecMap which contains the
+// nodes
+fn cantor_pairing(coords: (usize, usize)) -> usize {
+  let (x, y) = coords;
+  (((x + y) * (x+y+1)) / 2 + y) as usize
+}
+
+fn create_node(depth:usize, position:usize, val: i32) -> Node {
+    Node {
+        depth:depth,
+        position:position,
+        val:val,
+        children_max_sum: 0
     }
 }
 
-struct LiveNodes {
-    list: Vec<Node>
-}
-
-// very inefficient since I am reallocating memory on each action, pruning and generation but this
-// is just a first pass
-impl LiveNodes {
-    fn prune(self) -> LiveNodes {
-        // Taking advantage of the known ordering here to only do one loop of the nodes
-        let mut current_node = self.list[0].clone();
-
-        let mut live_nodes: Vec<Node> = Vec::new();
-        for i in self.list {
-            let Node {depth:depth, position:position, val:val, ..} = current_node;
-            if (i.max() > current_node.max() || i.position== 0 || i.position == i.depth) {
-                live_nodes.push(i.clone());
-            }
-            current_node = i;
-        }
-        LiveNodes { list: live_nodes}
-    }
-
-    fn maybeAddParent(&mut self, node: Option<Node>) {
-        match node {
-            None => (),
-            Some(x) => self.list.push(x),
-        }
-    }
-
-    fn generateParents(self, data: &Vec<Vec<i32>>) -> LiveNodes {
-        let mut live_nodes =  LiveNodes{list : Vec::new()};
-        for i in self.list {
-            live_nodes.maybeAddParent(i.getParent(true, data));
-            live_nodes.maybeAddParent(i.getParent(false, data));
-        }
-        live_nodes
-    }
+fn get_child_from_coords(coords: (usize, usize), path_map: &VecMap<Node>) -> &Node {
+    path_map.get(&cantor_pairing(coords)).unwrap()
 }
 
 fn main() {
-    let input: Vec<Vec<i32>> = "75
+let input: Vec<Vec<i32>> = "75
 95 64
 17 47 82
 18 35 87 10
@@ -102,21 +66,26 @@ fn main() {
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23".split('\n').map(|line| line.split(' '). map(|num| num.parse::<i32>().unwrap()).collect()).collect();
 
+    let mut current_depth: usize = input.len() - 1;
     let max_depth: usize = input.len()  - 1;
-    let mut live_nodes = LiveNodes { list: Vec::new()};
-    for i in range(0, max_depth) {
-        live_nodes.list.push(Node {
-            depth:max_depth,
-            position: i,
-            val: input[max_depth][i],
-            children_max_sum: 0,
-        })
-
+    let mut path_map = VecMap::new();
+    loop {
+        for i in range(0, current_depth + 1) {
+            let mut new_node = create_node(current_depth, i, input[current_depth][i]);
+            let children_max_sum = if current_depth < max_depth {
+                let left_child = get_child_from_coords(new_node.get_child_coords(true), &path_map);
+                let right_child = get_child_from_coords(new_node.get_child_coords(false), &path_map);
+                max(left_child.total(), right_child.total())
+            } else {
+                0
+            };
+            new_node.children_max_sum = children_max_sum;
+            if current_depth == 0 {
+                println!("Max path sum is {}", new_node.total());
+                return;
+            }
+            path_map.insert(cantor_pairing((current_depth, i)), new_node);
+        }
+        current_depth -= 1;
     }
-    while live_nodes.list.len() > 1 {
-        live_nodes = live_nodes.prune();
-        live_nodes = live_nodes.generateParents(&input);
-    }
-    let Node {depth: x,position: y,val: z, ..} = live_nodes.list[0];
-    println!("({},{}) = {}", x,y,z);
 }
